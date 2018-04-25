754f9d6bf2416c7ccdde4f156b21e75955c45bed
luisdramirez <ldr285@nyu.edu>
2018-04-25 16:07:00 -0400

    first outline of VMmf task.

--- run_VMmf.m
+++ run_VMmf.m
@@ -1,1 +1,29 @@
-% visual memory multiple features
+%%% run_VMmf.m 
+%%% Visual Memory Task: Perception and Memory of multiple stimulus features.
+%%%     Features: location, contrast, and orientation.
+%%%     Surround Suppression: orthogonal and collinear surround (to be included later)
+%%%     Task: reconstruct target presented through fixed method of adjustment (knob)       
+%%% Written by Luis Ramirez and Julia Schwartz 
+
+%% PREPARE *
+
+%% SCREEN PARAMETERS
+
+%% GRATING PARAMETERS *
+
+%% MASK PARAMETERS
+
+%% TRIAL EVENTS **
+
+
+%% TIMING PARAMETERS
+
+%% CREATE STIMULI
+
+%% WINDOW SETUP
+
+%% INITIALIZE QUEST
+
+%% EXPERIMENT **
+
+%% SAVE DATA 
