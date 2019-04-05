dd9b409fb76b378b6f7cc873ce0f6014a9f37da9
luisdramirez <ldr285@nyu.edu>
2019-04-05 16:39:31 -0400

    Update vmmf_subject_analysis_v2.m
    analysis script

--- vmmf_subject_analysis_v2.m
+++ vmmf_subject_analysis_v2.m
@@ -5,49 +5,44 @@ clear all; close all; clc;
 scrsz = get(groot,'ScreenSize');
 expDir=pwd;
 dataDir='data_master';
-subjects = 1;
-subjPlots = 0;
-groupPlots = 0;
 
-data = struct('TheData',[],'ContrastEstimates',[],'ProbeOffset',[]);
+subjects = 1;
+
+allData = struct([]);
 
 % Load in data
 cd(dataDir)
-
 for nSubj = subjects
     fileName = ['data_vmmf_v2_' num2str(nSubj) '.mat'];
     if exist(fileName,'file') ~= 0
         load(fileName);
     end
-    data.TheData{nSubj} = theData;
+    allData{nSubj} = theData;
     clear theData
 end
-data.ContrastEstimates = nan(length(subjects),length(data.TheData{1, 1}.p.centerContrasts)); % initialize average contrast estimates
-
 cd(expDir)
 %% Data organization
 
-targetContrasts = data.TheData{1}.p.centerContrasts;
-
+targetContrasts = allData{1}.p.centerContrasts;
 
 for nSubj = subjects
-    for runNum = 1:size(size(data.TheData{1, 1},1) % Number of runs
+    for nRun = 1:length(allData.TheData{nSubj}.data) % Number of runs
         for nCon = 1:length(targetContrasts) 
-            conindx = data.TheData{nSubj}.p.trialEvents(:,1) == targetContrasts(nCon);
+            conindx = allData{nSubj}.p.trialEvents(:,1) == targetContrasts(nCon);
             if subjPlots 
                 %Plots that graph a distribution for each contrast (Not
                 %considering probe offset)
                 figure(nSubj);
                 set(gcf,'Name',' Subject %i')
                 subplot(length(targetContrasts),1,nCon)
-                hist(data.TheData{nSubj}.data.EstimatedContrast(conindx),20);
+                hist(allData{nSubj}.data.EstimatedContrast(conindx),20);
                 title(sprintf('Contrast Level: %.2f',targetContrasts(nCon)))
-                data.ContrastEstimates(nSubj,nCon) = mean(data.TheData{nSubj}.data.EstimatedContrast(conindx));
+                allData.ContrastEstimates(nSubj,nCon) = mean(allData{nSubj}.data.EstimatedContrast(conindx));
                 xlabel('Contrast');
                 ylabel('Instances');
             end
-            estContrast(:,nCon,nSubj) = data.TheData{nSubj}.data.EstimatedContrast(conindx);
-            probeOffsetTemp = data.TheData{nSubj}.p.trialEvents(find(conindx == 1),4) - data.TheData{nSubj}.p.trialEvents(find(conindx == 1),2);
+            estContrast(:,nCon,nSubj) = allData{nSubj}.data.EstimatedContrast(conindx);
+            probeOffsetTemp = allData.TheData{nSubj}.p.trialEvents(find(conindx == 1),4) - allData{nSubj}.p.trialEvents(find(conindx == 1),2);
             probeOffsetTemp(probeOffsetTemp > 180) = probeOffsetTemp(probeOffsetTemp > 180)-360;
             probeOffsetTemp(probeOffsetTemp < -180) = 360+probeOffsetTemp(probeOffsetTemp < -180);
             probeOffset(:,nCon,nSubj) = probeOffsetTemp;
